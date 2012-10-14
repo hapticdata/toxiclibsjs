ed286695823f7883143c214b82b1fba8e3b41c1f
Kyle Phillips <kyle@haptic-data.com>
2012-10-14 12:22:44 -0500

    ported WETriangleMesh, WEFace, WEVertex, WingedEdge

---
+++ tests/mocha/TriangleMesh.js
@@ -0,0 +1,62 @@
+var toxi = require('../../index'),
+	assert = require('assert');
+
+describe('TriangleMesh', function(){
+
+	function testPositionAndExtent( instance, pos, extentNumber ){
+		it(['should have a centroid of ',pos.x, pos.y, pos.z].join(','), function(){
+			var c = instance.computeCentroid();
+			assert.equal(c.x, pos.x);
+			assert.equal(c.y, pos.y);
+			assert.equal(c.z, pos.z);
+		});
+		it(['vertices should all be either -',extentNumber,' and ',extentNumber].join(','), function(){
+			var passed = true;
+			instance.vertices.forEach(function( v ){
+				['x','y','z'].forEach(function( axis ){
+					if( v[axis] !== (-extentNumber+pos[axis]) && v[axis] !== (extentNumber+pos[axis])){
+						console.log('v['+axis+'] = '+ v[axis]);
+						passed = false;
+					}
+				});
+			});
+			assert.equal(passed, true);
+		});
+	}
+
+	describe('from AABB', function(){
+		var aabb = new toxi.geom.AABB( new toxi.geom.Vec3D(), new toxi.geom.Vec3D(1,1,1) );
+		var instance = aabb.toMesh();
+		describe('default',function(){
+			it('should be a TriangleMesh', function(){
+				assert.equal(instance instanceof toxi.geom.mesh.TriangleMesh, true);
+				assert.equal(typeof instance.computeCentroid, 'function');
+			});
+			it('should have 8 points', function(){
+				assert.equal(instance.getNumVertices(), 8);
+			});
+			it('should have 12 faces', function(){
+				assert.equal(instance.getNumFaces(), 12);
+			});
+			testPositionAndExtent( instance, {x: 0, y: 0, z: 0}, 1);
+		});
+
+		describe('#transform()', function(){
+			var instance2 = instance.copy();
+			var matrix = new toxi.geom.Matrix4x4();
+			matrix.translateSelf( 1, 1, 1);
+			matrix.scaleSelf( 2, 2, 2 );
+			instance2.transform( matrix );
+
+			testPositionAndExtent( instance2, {x: 1, y: 1, z: 1}, 2);
+		});
+
+		describe('#toWEMesh()', function(){
+			instance.toWEMesh(function( wemesh ){
+				it('should be a WETriangleMesh instance', function(){
+					assert.equal( wemesh instanceof toxi.geom.mesh.WETriangleMesh, true );
+				});
+			});
+		});
+	});
+});
