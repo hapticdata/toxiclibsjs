4eceecbd2004bdbde14e9da261b44838a5919b55
Kyle Phillips <kylephillips@google.com>
2015-12-01 23:31:19 -0800

    building into commonjs folder and ignoring it

--- bin/convert-to-commonjs.js
+++ bin/convert-to-commonjs.js
@@ -2,18 +2,18 @@ var nodefy = require('nodefy'),
     fs = require('fs'),
     path = require('path');
 
+var target = 'commonjs';
 
 
 
-nodefy.batchConvert('lib/toxi/**/*.js', 'cjs', function(err, results){
+nodefy.batchConvert('lib/toxi/**/*.js', target, function(err, results){
     if(err){
         console.log(err);
     }
 
     console.log(results.length + ' files converted');
 
-    var cjsPath = path.resolve('cjs');
-    fs.createReadStream('package.json').pipe(fs.createWriteStream(path.join('cjs','package.json')));
+    fs.createReadStream('package.json').pipe(fs.createWriteStream(path.join(target,'package.json')));
 });
 
 
