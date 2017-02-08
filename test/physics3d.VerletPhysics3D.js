c686a2ff14370d866419bdbb6c28ba78235e0beb
Kyle Phillips <kylephillips@google.com>
2017-02-08 12:27:15 -0800

    fixes VerletPhysics3D#getSpring and applyConstraints
    allows VerletPhysics3D constructor to accept options argument

--- test/physics3d.VerletPhysics3D.js
+++ test/physics3d.VerletPhysics3D.js
@@ -37,42 +37,42 @@ describe("toxi.physics3d.VerletPhysics3D", function(){
 			});
 			testDefaults( p );
 		});
-		describe("1 param gravity behavior", function(){
-			var grav = new GravityBehavior( new Vec3D( 0, 0.1) );
-			var p = new VerletPhysics3D( grav );
-			testGravity( p );
-			testDefaults( p );
-		});
-		describe("1 param, Vec2D as gravity", function(){
-			var p = new VerletPhysics3D( new Vec3D( 0, 0.1 ) );
+		// describe("1 param gravity behavior", function(){
+		// 	var grav = new GravityBehavior( new Vec3D( 0, 0.1) );
+		// 	var p = new VerletPhysics3D( grav );
+		// 	testGravity( p );
+		// 	testDefaults( p );
+		// });
+		describe("1 param, Vec3D as gravity", function(){
+			var p = new VerletPhysics3D( new Vec3D( 0, 0.1, 0 ) );
 			testDefaults( p );
 			testGravity( p );
 		});
 		describe("1 param, options object, all properties", function(){
 			var p = new VerletPhysics3D({
-				gravity: new Vec3D(0.1,-1),
+				gravity: new Vec3D(0.1,-1, 0),
 				drag: 0.05,
 				timeStep: 2,
 				numIterations: 25
 			});
 			testGravity( p );
 			it('should have defined properties', function(){
-				assert.equal( p.drag, 1.0 - 0.05 );
+				assert.ok( Math.abs(p.getDrag() - 0.05) < 0.00001 ); //floating-point precision issue
 				assert.equal( p.getTimeStep(), 2 );
-				assert.equal( p.numIterations, 25 );
+				assert.equal( p.getNumIterations(), 25 );
 			});
 		});
 
 		describe("1 param, options object w/ drag and timeStep", function(){
-			var p = new VerletPhysics2D({
+			var p = new VerletPhysics3D({
 				drag: 0.05,
 				timeStep: 2
 			});
 			it("should have set properties and default others", function(){
 				assert.equal( p.behaviors.length, 0 );
-				assert.equal( p.drag, 1.0 - 0.05 );
-				assert.equal( p.timeStep, 2 );
-				assert.equal( p.numIterations, 50 );
+        assert.ok( Math.abs(p.getDrag() - 0.05) < 0.00001 ); //floating-point precision issue
+				assert.equal( p.getTimeStep(), 2 );
+				assert.equal( p.getNumIterations(), 50 );
 			});
 		});
 	});
