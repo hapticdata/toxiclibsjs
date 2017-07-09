cfd649442be4f470d9f1c5ebdb456ca3aec20baf
Daniel Worsnup <daniel.worsnup@forrent.com>
2017-07-09 16:04:26 -0400

    Write unit tests around Ellipse#containsPoint

--- test/geom.Ellipse.js
+++ test/geom.Ellipse.js
@@ -14,4 +14,60 @@ describe('toxi.geom.Ellipse', function(){
 			});
 		});
 	});
+    describe('#containsPoint', function(){
+        describe('tall ellipse', function(){
+             var ellipse = new toxi.geom.Ellipse(100, 100, 20, 50);
+            it('should return true for the center', function(){
+                var p = new toxi.geom.Vec2D(100, 100);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return true for an internal point', function(){
+                var p = new toxi.geom.Vec2D(110, 110);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return true for a point inside the top edge', function(){
+                var p = new toxi.geom.Vec2D(100, 149.9);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return true for a point inside the right edge', function(){
+                var p = new toxi.geom.Vec2D(119.9, 100);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return false for a point outside the ellipse', function(){
+                var p = new toxi.geom.Vec2D(119, 149);
+                assert.equal(ellipse.containsPoint(p), false);
+            });
+            it('should return false for a point outside the containing rectangle of the ellipse', function(){
+                var p = new toxi.geom.Vec2D(125, 155);
+                assert.equal(ellipse.containsPoint(p), false);
+            });
+        });
+        describe('wide ellipse', function(){
+             var ellipse = new toxi.geom.Ellipse(100, 100, 50, 20);
+            it('should return true for the center', function(){
+                var p = new toxi.geom.Vec2D(100, 100);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return true for an internal point', function(){
+                var p = new toxi.geom.Vec2D(110, 110);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return true for a point inside the top edge', function(){
+                var p = new toxi.geom.Vec2D(100, 119.9);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return true for a point inside the right edge', function(){
+                var p = new toxi.geom.Vec2D(149.9, 100);
+                assert.equal(ellipse.containsPoint(p), true);
+            });
+            it('should return false for a point outside the ellipse', function(){
+                var p = new toxi.geom.Vec2D(149, 119);
+                assert.equal(ellipse.containsPoint(p), false);
+            });
+            it('should return false for a point outside the containing rectangle of the ellipse', function(){
+                var p = new toxi.geom.Vec2D(155, 125);
+                assert.equal(ellipse.containsPoint(p), false);
+            });
+        });
+    });
 });
